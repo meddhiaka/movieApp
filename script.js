9a1458cf87046f38347a6a6bbe17824b31982370
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2022-10-11 19:47:28 +0100

    Uploading the complete movie app

---
+++ script.js
@@ -0,0 +1,78 @@
+const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ac461d57043fa2e6902eeb13a1c58656&page=1"
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=ac461d57043fa2e6902eeb13a1c58656&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+// get initial movies
+getMovies(API_URL)
+
+async function getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ""
+    
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average } = movie
+        
+        movieElement = document.createElement('div')
+        movieElement.classList.add('movie')
+
+        movieElement.innerHTML = `
+        
+
+            <img src="${ IMG_PATH + poster_path }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="green">${vote_average}</span>
+            </div>
+            <div class="${getClassByRate(vote_average)}" style="padding: 1rem;">
+                <h3>Overview</h3>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae est ex atque delectus maiores qui nesciunt corporis soluta voluptate illo?
+            </div>
+
+        
+        `
+
+        main.appendChild(movieElement)
+    });
+}
+
+function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green'
+    }
+    else if(vote >= 5){
+        return 'orange'
+    }
+    else
+    {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !==''){
+        getMovies(SEARCH_URL + searchTerm)
+
+        search.value = ''
+    }else{
+        window.location.reload()
+    }
+
+
+
+})
